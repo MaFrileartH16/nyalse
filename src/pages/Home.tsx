a6f4df54f29a435951d631cd7fff8fe63f8cff30
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-04-08 11:02:57 +0700

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,31 +5,31 @@ import {useRef} from "react";
 
 const mostPickedData = [
 	{
-		image: "",
+		image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 		title: "Blue Origin Fams",
 		location: "Jakarta, Indonesia",
 		price: "$50 per night"
 	},
 	{
-		image: "",
+		image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 		title: "Londa",
 		location: "Bandung, Indonesia",
 		price: "$22 per night"
 	},
 	{
-		image: "",
+		image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 		title: "Stark House",
 		location: "Karawang, Indonesia",
 		price: "$856 per night"
 	},
 	{
-		image: "",
+		image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 		title: "Stark House",
 		location: "Karawang, Indonesia",
 		price: "$856 per night"
 	},
 	{
-		image: "",
+		image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 		title: "Stark House",
 		location: "Karawang, Indonesia",
 		price: "$856 per night"
@@ -41,24 +41,51 @@ const propertyCategoriesData = [
 		title: "Houses with beauty backyard",
 		properties: [
 			{
-				image: "",
+				image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 				title: "Tabby Town",
 				location: "Gunung Batu, Indonesia",
 				popular: true
 			},
 			{
-				image: "",
+				image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 				title: "Anggana",
 				location: "Bogor, Indonesia"
 			},
 			{
-				image: "",
+				image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 				title: "Tabby Town",
 				location: "Gunung Batu, Indonesia",
 				popular: true
 			},
 			{
-				image: "",
+				image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+				title: "Anggana",
+				location: "Bogor, Indonesia"
+			}
+		]
+	},
+	{
+		title: "Houses with beauty backyard",
+		properties: [
+			{
+				image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+				title: "Tabby Town",
+				location: "Gunung Batu, Indonesia",
+				popular: true
+			},
+			{
+				image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+				title: "Anggana",
+				location: "Bogor, Indonesia"
+			},
+			{
+				image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+				title: "Tabby Town",
+				location: "Gunung Batu, Indonesia",
+				popular: true
+			},
+			{
+				image: 'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 				title: "Anggana",
 				location: "Bogor, Indonesia"
 			}
